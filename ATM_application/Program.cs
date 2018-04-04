2ce6065eae0b3a610b9d721151f73d6e9b02cb2c
LM <LM@LRKEMADSEN964D>
2018-04-04 12:03:11 +0200

    App projekt og ny Track-klasse

---
+++ ATM_application/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransponderReceiver;
+
+namespace ATM_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myReciever = TransponderReceiverFactory.CreateTransponderDataReceiver();
+
+            myReciever.TransponderDataReady += MyReciever_TransponderDataReady;
+
+            Console.ReadKey();
+        }
+
+        private static void MyReciever_TransponderDataReady(object sender, RawTransponderDataEventArgs e)
+        {
+            var myList = e.TransponderData;
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);   
+            }
+        }
+    }
+}
---
+++ AirTrafficMonitoring/ConvertTrackData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirTrafficMonitoring
+{
+    class ConvertTrackData
+    {
+
+        public Track ConvertData(string trackData)
+        {
+            var myTrack = new Track();
+
+
+
+            return myTrack;
+        }
+    }
+}

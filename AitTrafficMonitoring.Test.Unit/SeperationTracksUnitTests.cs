e2662af49d3f33a6b05c543aeb5bf8769bc4cec8
LM <LM@LRKEMADSEN964D>
2018-04-19 13:45:27 +0200

    Ændret i FilterAirspace, så det er en property, som bliver sat

--- AitTrafficMonitoring.Test.Unit/SeperationTracksUnitTests.cs
+++ AitTrafficMonitoring.Test.Unit/SeperationTracksUnitTests.cs
@@ -23,6 +23,9 @@ namespace AitTrafficMonitoring.Test.Unit
         private DateTime _date2;
         private Track _track1;
         private Track _track2;
+        private Track _track3;
+        private DateTime _date3;
+        private DateTime _date4;
 
         [SetUp]
         public void Setup()
@@ -34,13 +37,16 @@ namespace AitTrafficMonitoring.Test.Unit
 
             _date1 = new DateTime(2018, 4, 17, 20, 15, 12);
             _date2 = new DateTime(2018, 4, 17, 20, 15, 14);
+            _date3 = new DateTime(2018, 4, 17, 20, 16, 26);
+            _date4 = new DateTime(2018, 4, 17, 20, 17, 21);
+
 
             _track1 = new Track
             {
                 Tag = "BIJ515",
                 XCoordinate = 12345,
                 YCoordinate = 54321,
-                Altitude = 17891,
+                Altitude = 17000,
                 Timestamp = _date1
             };
 
@@ -49,19 +55,29 @@ namespace AitTrafficMonitoring.Test.Unit
                 Tag = "QWF639",
                 XCoordinate = 12346,
                 YCoordinate = 54322,
-                Altitude = 17892,
+                Altitude = 17299,
                 Timestamp = _date2
+            };
+
+            _track3 = new Track()
+            {
+                Tag = "PXL968",
+                XCoordinate = 12346,
+                YCoordinate = 54322,
+                Altitude = 17299,
+                Timestamp = _date3
             };
 
             _myList = new List<Track>
             {
                 _track1,
-                _track2
+                _track2,
+                _track3
             };
         }
 
         [Test]
-        public void SeperationCheck_ToTracksCollide_LogToFileAndConsole()
+        public void SeperationCheck_VerticalSeparationUnder300_LogToFileAndConsole()
         {
             _uut.SeperationCheck(_myList);
 
@@ -70,9 +86,65 @@ namespace AitTrafficMonitoring.Test.Unit
         }
 
         [Test]
-        public void SeperationCheck_ToTracksDoNotCollide_NoSeperationEvent()
+        public void SeperationCheck_VerticalSeparationAbove300_NoSeperationEvent()
         {
-            _track1.Altitude = 17592;
+            _track2.Altitude = 17300;
+
+            _uut.SeperationCheck(_myList);
+
+            _logWriterToFile.DidNotReceive().LogEvent(_track1.Tag, _track2.Tag, _date2);
+            _logWriterToConsole.DidNotReceive().LogEvent(_track1.Tag, _track2.Tag, _date2);
+        }
+
+        [Test]
+        public void SeperationCheck_HorizontalSeparationUnder5000_LogToFileAndConsole()
+        {
+            _track2.XCoordinate = 17344;
+            _track2.YCoordinate = 54321;
+
+            _uut.SeperationCheck(_myList);
+
+            _logWriterToFile.Received().LogEvent(_track1.Tag, _track2.Tag, _date2);
+            _logWriterToConsole.Received().LogEvent(_track1.Tag, _track2.Tag, _date2);
+        }
+
+        [Test]
+        public void SeperationCheck_HorizontalSeparationAbove5000_NoSeperationEvent()
+        {
+            _track2.Altitude = 17300;
+            _track2.XCoordinate = 17345;
+            _track2.YCoordinate = 54321;
+
+            _uut.SeperationCheck(_myList);
+
+            _logWriterToFile.DidNotReceive().LogEvent(_track1.Tag, _track2.Tag, _date2);
+            _logWriterToConsole.DidNotReceive().LogEvent(_track1.Tag, _track2.Tag, _date2);
+        }
+
+        [Test]
+        public void SeperationCheck_Track1AndTrack3Collide_LogToFileAndConsole()
+        {
+            _uut.SeperationCheck(_myList);
+
+            _logWriterToFile.Received().LogEvent(_track1.Tag, _track3.Tag, _date3);
+            _logWriterToConsole.Received().LogEvent(_track1.Tag, _track3.Tag, _date3);
+        }
+
+        [Test]
+        public void SeperationCheck_Track1AndTrack3CollideWithLatestDateTime_LogToFileAndConsole()
+        {
+            _track1.Timestamp = _date4;
+
+            _uut.SeperationCheck(_myList);
+
+            _logWriterToFile.Received().LogEvent(_track1.Tag, _track3.Tag, _date4);
+            _logWriterToConsole.Received().LogEvent(_track1.Tag, _track3.Tag, _date4);
+        }
+
+        [Test]
+        public void SeperationCheck_SameTag_NoSeperationEvent()
+        {
+            _track2.Tag = "BIJ515";
 
             _uut.SeperationCheck(_myList);
 

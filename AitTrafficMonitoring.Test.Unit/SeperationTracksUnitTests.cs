7f04414c98e448bf7c5cca7d53216c1012cf32da
LM <LM@LRKEMADSEN964D>
2018-04-24 09:40:01 +0200

    Nye tests

--- AitTrafficMonitoring.Test.Unit/SeperationTracksUnitTests.cs
+++ AitTrafficMonitoring.Test.Unit/SeperationTracksUnitTests.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using AirTrafficMonitoring;
 using NSubstitute;
+using NSubstitute.Core.Arguments;
 using NUnit.Framework;
 using TransponderReceiver;
 
@@ -15,10 +17,7 @@ namespace AitTrafficMonitoring.Test.Unit
     {
         private ISeperationTracks _uut;
         private SeperationEvent _seperationEvent;
-        private List<SeperationEvent> _seperationList;
         private IEventController _eventController;
-        //private ILogWriter _logWriterToFile;
-        //private ILogWriter _logWriterToConsole;
         private List<Track> _myList;
         private DateTime _date1;
         private DateTime _date2;
@@ -26,7 +25,6 @@ namespace AitTrafficMonitoring.Test.Unit
         private Track _track2;
         private Track _track3;
         private DateTime _date3;
-        private DateTime _date4;
 
         [SetUp]
         public void Setup()
@@ -38,7 +36,6 @@ namespace AitTrafficMonitoring.Test.Unit
             _date1 = new DateTime(2018, 4, 17, 20, 15, 12);
             _date2 = new DateTime(2018, 4, 17, 20, 15, 14);
             _date3 = new DateTime(2018, 4, 17, 20, 16, 26);
-            _date4 = new DateTime(2018, 4, 17, 20, 17, 21);
 
             _track1 = new Track
             {
@@ -58,23 +55,20 @@ namespace AitTrafficMonitoring.Test.Unit
                 Timestamp = _date2
             };
 
-            //_track3 = new Track()
-            //{
-            //    Tag = "PXL968",
-            //    XCoordinate = 12346,
-            //    YCoordinate = 54322,
-            //    Altitude = 17299,
-            //    Timestamp = _date3
-            //};
+            _track3 = new Track()
+            {
+                Tag = "PXL968",
+                XCoordinate = 12346,
+                YCoordinate = 54322,
+                Altitude = 17299,
+                Timestamp = _date3
+            };
 
             _myList = new List<Track>
             {
                 _track1,
-                _track2,
-                //_track3
+                _track2
             };
-            _seperationList = new List<SeperationEvent>();
-            _seperationList.Add(_seperationEvent);
         }
 
 
@@ -83,7 +77,7 @@ namespace AitTrafficMonitoring.Test.Unit
         {
             _uut.SeperationCheck(_myList);
 
-            _eventController.Received().seperationsDetected(_seperationList);
+            _eventController.Received().seperationsDetected(Arg.Is<List<SeperationEvent>>((x) => x[0].Tag1 == "BIJ515" && x[0].Tag2 == "QWF639"));
         }
 
         [Test]
@@ -93,7 +87,53 @@ namespace AitTrafficMonitoring.Test.Unit
 
             _uut.SeperationCheck(_myList);
 
-            _eventController.DidNotReceive().seperationsDetected(_seperationList);
+            _eventController.DidNotReceive().seperationsDetected(Arg.Any<List<SeperationEvent>>());
+        }
+
+        [Test]
+        public void SeperationCheck_HorizontalSeparationUnder5000_LogToFileAndConsole()
+        {
+            _track2.XCoordinate = 17344;
+            _track2.YCoordinate = 54321;
+
+            _uut.SeperationCheck(_myList);
+
+            _eventController.Received().seperationsDetected(Arg.Is<List<SeperationEvent>>((x) => x[0].Tag1 == "BIJ515" && x[0].Tag2 == "QWF639"));
+        }
+
+        [Test]
+        public void SeperationCheck_HorizontalSeparationAbove5000_NoSeperationEvent()
+        {
+            _track2.Altitude = 17300;
+            _track2.XCoordinate = 17345;
+            _track2.YCoordinate = 54321;
+
+            _uut.SeperationCheck(_myList);
+
+            _eventController.DidNotReceive().seperationsDetected(Arg.Any<List<SeperationEvent>>());
+        }
+
+        [Test]
+        public void SeperationCheck_Track1AndTrack3Collide_LogToFileAndConsole()
+        {
+            _track2.Altitude = 18000;
+
+            _myList.Add(_track3);
+
+            _uut.SeperationCheck(_myList);
+
+            _eventController.Received().seperationsDetected(Arg.Is<List<SeperationEvent>>((x) => x[0].Tag1 == "BIJ515" && x[0].Tag2 == "PXL968"));
+
+        }
+
+        [Test]
+        public void SeperationCheck_SameTag_NoSeperationEvent()
+        {
+            _track2.Tag = "BIJ515";
+
+            _uut.SeperationCheck(_myList);
+
+            _eventController.DidNotReceive().seperationsDetected(Arg.Any<List<SeperationEvent>>());
         }
     }
 }
